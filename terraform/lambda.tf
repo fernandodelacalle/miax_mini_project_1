50497c0c1ebc4526556731f71a2fb92771db941e
Fernando <fernando.decalle@gmail.com>
2024-03-23 11:48:58 +0100

    lambda

---
+++ terraform/lambda.tf
@@ -0,0 +1,46 @@
+data aws_iam_policy_document lambda_assume_role {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+  }
+}
+
+resource aws_iam_role lambda_role {
+  name               = "lambda-role"
+  assume_role_policy = data.aws_iam_policy_document.lambda_assume_role.json
+}
+
+data aws_iam_policy_document lambda_s3 {
+  statement {
+    actions = [
+      "s3:PutObject",
+      "s3:PutObjectAcl"
+    ]
+
+    resources = [
+      "arn:aws:s3:::bucket/*"
+    ]
+  }
+}
+
+resource aws_iam_policy lambda_s3 {
+  name        = "lambda-s3-permissions"
+  description = "Contains S3 put permission for lambda"
+  policy      = data.aws_iam_policy_document.lambda_s3.json
+}
+
+resource aws_iam_role_policy_attachment lambda_s3 {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.lambda_s3.arn
+}
+
+resource "aws_lambda_function" "executable" {
+  function_name = "market_data_download"
+  image_uri     = "${aws_ecr_repository.api_repository.repository_url}:38283ad1d8d5eb910ba32cefab747c5439c8ec35"
+  package_type  = "Image"
+  role          = aws_iam_role.lambda_role.arn
+}
