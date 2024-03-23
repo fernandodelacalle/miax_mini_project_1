69ce7146a9818442127d93e29dd5d118a77f0d7d
Fernando <fernando.decalle@gmail.com>
2024-03-23 10:30:39 +0100

    provider

---
+++ terraform/main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+  backend "s3" {
+      bucket = "tfstatemiax"
+      key    = "miniproject1"
+      region = "eu-west-1"
+  }
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
